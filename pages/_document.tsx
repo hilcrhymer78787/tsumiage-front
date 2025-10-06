9291c36a0682616c2f95902d82c9c845a4519177
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-10-06 20:16:53 +0900

    ⭐️余計なアイコンの削除

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,85 +5,25 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Windows */}
-          <meta name="msapplication-square70x70logo" content="/site-tile-70x70.png" />
-          <meta name="msapplication-square150x150logo" content="/site-tile-150x150.png" />
-          <meta name="msapplication-wide310x150logo" content="/site-tile-310x150.png" />
-          <meta name="msapplication-square310x310logo" content="/site-tile-310x310.png" />
-          <meta name="msapplication-TileColor" content="#000" />
-          {/* Safari */}
+          {/* Favicon */}
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" sizes="32x32" href="/icon-32x32.png" />
+
+          {/* PWA Manifest */}
+          <link rel="manifest" href="/manifest.json" />
+
+          {/* Apple */}
+          <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="#222222" />
           <meta name="apple-mobile-web-app-title" content="tsumiage" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
-          {/* Splash screens */}
-          <link
-            href="splashscreens/iphone5_splash.png"
-            media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/iphone6_splash.png"
-            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/iphoneplus_splash.png"
-            media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/iphonex_splash.png"
-            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/iphonexr_splash.png"
-            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/iphonexsmax_splash.png"
-            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/ipad_splash.png"
-            media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/ipadpro1_splash.png"
-            media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/ipadpro3_splash.png"
-            media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          <link
-            href="splashscreens/ipadpro2_splash.png"
-            media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
-            rel="apple-touch-startup-image"
-          />
-          {/* General */}
-          <meta name="application-name" content="tsumiage" />
+
+          {/* Theme */}
           <meta name="theme-color" content="#000" />
           <meta name="description" content="this is tsumiage" />
-          <link rel="icon" sizes="192x192" href="/icon-192x192.png" />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script
-            async
-            src="https://cdn.jsdelivr.net/npm/pwacompat@2.0.6/pwacompat.min.js"
-            integrity="sha384-GOaSLecPIMCJksN83HLuYf9FToOiQ2Df0+0ntv7ey8zjUHESXhthwvq9hXAZTifA"
-            crossOrigin="anonymous"
-          ></script>
         </body>
       </Html>
     );
