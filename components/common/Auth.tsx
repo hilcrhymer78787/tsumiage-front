930b8bea2ae03b432e14fa9aa34796aa21174ecf
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-10-07 16:24:40 +0900

    fix

--- components/common/Auth.tsx
+++ components/common/Auth.tsx
@@ -50,20 +50,20 @@ const Auth = ({ setIsNew }: { setIsNew: Dispatch<SetStateAction<boolean>> }) => 
               label="password"
             />
             <ErrTxt txt={message} p={0} />
-            {process.env.NEXT_PUBLIC_APP_ENV !== "production" && (
-              <RStack justifyContent="flex-end">
-                <LoadingButton
-                  color="inherit"
-                  variant="contained"
-                  onClick={testAuth}
-                  loading={testAuthLoading}
-                  disabled={basicAuthLoading}
-                >
-                  テストユーザーでログイン
-                  <SendIcon />
-                </LoadingButton>
-              </RStack>
-            )}
+            {/* {process.env.NEXT_PUBLIC_APP_ENV !== "production" && ( */}
+            <RStack justifyContent="flex-end">
+              <LoadingButton
+                color="inherit"
+                variant="contained"
+                onClick={testAuth}
+                loading={testAuthLoading}
+                disabled={basicAuthLoading}
+              >
+                テストユーザーでログイン
+                <SendIcon />
+              </LoadingButton>
+            </RStack>
+            {/* )} */}
           </Stack>
           <CardActions>
             <Button onClick={() => setIsNew(true)} color="inherit" variant="contained">
