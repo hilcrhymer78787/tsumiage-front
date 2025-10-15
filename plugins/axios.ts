3eb74979dec6711844d3b6e41af0c02ae241f261
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-10-15 16:33:44 +0900

    fix

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -2,13 +2,12 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
 });
 
 api.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (!req.headers) return req;
-  req.headers.Accept = "application/json";
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
