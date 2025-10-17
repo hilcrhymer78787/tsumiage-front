fa7c77db52856b06f15377a83123f434590d2755
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2025-10-17 17:42:20 +0900

    fix

--- components/mypage/MypageMain.tsx
+++ components/mypage/MypageMain.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardActions,
@@ -8,6 +9,7 @@ import {
   ListItem,
   ListItemAvatar,
   ListItemText,
+  Stack,
 } from "@mui/material";
 import { useState } from "react";
 
@@ -25,33 +27,49 @@ const MypageMain = () => {
     if (!confirm("ログアウトしますか？")) return;
     logout();
   };
+
+  const onClickEmailVerify = () => {
+    if (!confirm("メール認証を行いますか？")) return;
+    //
+  };
+
   return (
-    <Card>
-      <CardHeader title="マイページ" />
-      <CardContent>
-        <ListItem sx={{ p: 0 }}>
-          <ListItemAvatar>
-            <UserImg fileName={loginInfo?.user_img} size="70" />
-          </ListItemAvatar>
-          <ListItemText
-            sx={{ ml: "15px" }}
-            primary={loginInfo?.name}
-            secondary={loginInfo?.email}
-          />
-        </ListItem>
-      </CardContent>
-      <CardActions>
-        <Button onClick={onClickLogout} color="inherit" variant="contained">
-          ログアウト
-        </Button>
-        <Button onClick={() => setIsOpen(true)} variant="contained">
-          編集
-        </Button>
-      </CardActions>
+    <>
+      <Card>
+        <CardHeader title="マイページ" />
+        <CardContent>
+          <Stack alignItems="flex-start" gap={3}>
+            <Box>
+              <ListItem sx={{ p: 0 }}>
+                <ListItemAvatar>
+                  <UserImg fileName={loginInfo?.user_img} size="70" />
+                </ListItemAvatar>
+                <ListItemText
+                  sx={{ ml: "15px" }}
+                  primary={loginInfo?.name}
+                  secondary={loginInfo?.email}
+                />
+              </ListItem>
+            </Box>
+            {/* TODO */}
+            {/* <Button onClick={onClickEmailVerify}>メール認証を行う</Button> */}
+          </Stack>
+        </CardContent>
+        <CardActions>
+          <Button onClick={onClickLogout} color="inherit" variant="contained">
+            ログアウト
+          </Button>
+          <Button onClick={() => setIsOpen(true)} variant="contained">
+            編集
+          </Button>
+        </CardActions>
+      </Card>
+      {process.env.NODE_ENV === "development" && <pre>{JSON.stringify(loginInfo, null, 4)}</pre>}
+
       <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
         {isOpen && <CreateUser loginInfo={loginInfo} onCloseMyself={() => setIsOpen(false)} />}
       </Dialog>
-    </Card>
+    </>
   );
 };
 export default MypageMain;
